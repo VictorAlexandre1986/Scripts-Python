341cbcb386f86656953e8e16226a9c9bd97d46db
Victor Alexandre <76669738+VictorAlexandre1986@users.noreply.github.com>
2021-10-18 20:35:44 -0300

    Add files via upload

---
+++ GerarCodigoBarras.py
@@ -0,0 +1,20 @@
+#pip install python-barcode
+
+from barcode import EAN13
+from barcode.writer import ImageWriter
+
+
+
+
+codigos_produtos = {
+    "Feijão":"553214569874",
+    "Arroz" :"123456789101",
+    "Macarrao":"564895521455",
+    "Azeite"  :"444523669874"
+}
+
+for produto in codigos_produtos:
+    codigo=codigos_produtos[produto]
+    #Precisa de doze digitos
+    codigo_barra = EAN13(codigo,writer=ImageWriter())
+    codigo_barra.save(f"codigo_barra_{produto}")
