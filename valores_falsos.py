6975a63a4cafd8bd5588ec54c9f673e6798a7e14
Victor Alexandre <76669738+VictorAlexandre1986@users.noreply.github.com>
2023-01-27 19:35:11 -0300

    Add files via upload

---
+++ valores_falsos.py
@@ -0,0 +1,12 @@
+#Todos as variaveis abaixo são consideradas falso
+
+lista = list()
+dicionario = dict()
+conjunto = set()
+tupla = tuple()
+string = ''
+inteiro = 0
+flutuante = 0.0
+nada = None
+falso = False
+intervalo = range(0)
